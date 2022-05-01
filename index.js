486bc7eac59a7b8b03c24579c0e31781fd842c58
ZhuangYunLiangCoder <1129697645@qq.com>
2022-05-01 23:50:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
 const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
 const progressBarOfThisYear = generateProgressBar()
 
+
 function generateProgressBar() {
     const progressBarCapacity = 30
     const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)

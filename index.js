d155dd8e8c731028695e1b179784618f9cf55e1d
ZhuangYunLiangCoder <1129697645@qq.com>
2022-05-01 23:53:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
 const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
 const progressBarOfThisYear = generateProgressBar()
 
-
 function generateProgressBar() {
     const progressBarCapacity = 30
     const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
@@ -16,9 +15,13 @@ function generateProgressBar() {
 
 const readme = `\
 ### Hi there 👋
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
+⏳ Year progress { █████████▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ } 31.92 %
+
 ---
-⏰ Updated on ${new Date().toUTCString()}
+
+⏰ Updated on Wed, 27 Apr 2022 12:13:51 GMT
+
 ---
 
 ### About me      
